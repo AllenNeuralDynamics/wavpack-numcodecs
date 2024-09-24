b2e35c4347b4ebceb4f5a2068d1699a8db83a1f3
Alessio Buccino <alejoe9187@gmail.com>
2024-09-24 18:10:19 +0200

    Fix wavpack version

--- .github/scripts/install-wavpack-linux.sh
+++ .github/scripts/install-wavpack-linux.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# Check if WAVPACK_VERSION argument is provided
+if [ -z "$1" ]; then
+    echo "Error: WAVPACK_VERSION argument is required."
+    exit 1
+else
+    WAVPACK_VERSION=$1
+fi
+
 # Check if TARGET_FOLDER argument is provided
-if [ -n "$1" ]; then
-    TARGET_FOLDER=$1
+if [ -n "$2" ]; then
+    TARGET_FOLDER=$2
     # Create the target folder if it doesn't exist
     mkdir -p "$TARGET_FOLDER"
     # Change directory to the target folder
@@ -13,11 +21,9 @@ sudo apt update
 sudo apt install wget
 sudo apt install -y gettext
 
-WAVPACK_LATEST_VERSION="$(cat ./.github/wavpack_latest_version.txt)"
-
-wget "https://www.wavpack.com/wavpack-$WAVPACK_LATEST_VERSION.tar.bz2"
-tar -xf wavpack-$WAVPACK_LATEST_VERSION.tar.bz2
-cd wavpack-$WAVPACK_LATEST_VERSION
+wget "https://www.wavpack.com/wavpack-$WAVPACK_VERSION.tar.bz2"
+tar -xf wavpack-$WAVPACK_VERSION.tar.bz2
+cd wavpack-$WAVPACK_VERSION
 ./configure
 sudo make install
 cd ..
